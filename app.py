2d990f3c78507f35ebd6e55965e866c42bfcea09
Jesús Pérez Torres <raviolink12@gmail.com>
2023-04-21 23:23:21 +0200

    commit all

--- app.py
+++ app.py
@@ -1,52 +1,67 @@
+import sqlite3
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
 import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import threading
 
 app = Flask(__name__)
-
-app.config['MONGO_DBNAME'] = 'app'
-app.config['MONGO_URI'] = 'mongodb+srv://jaeh:4_WgiEXiaHvb4Vy@cluster0.fgknz.mongodb.net/test'
-app.config['SECRET_KEY'] = 'secretkey'
-
-mongo = PyMongo(app)
+app.secret_key = 'secretkey'
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
 
-class User(UserMixin):
+class User:
     def __init__(self, username):
-        self.id = username
+        self.username = username
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.username
 
 @login_manager.user_loader
-def load_user(user_id):
-    u = mongo.db.usuarios.find_one({"nombre_usuario": user_id})
-    if not u:
-        return None
-    return User(u['nombre_usuario'])
+def load_user(username):
+    return User(username)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+def get_db():
+    if not hasattr(threading.current_thread(), 'sqlite_db'):
+        threading.current_thread().sqlite_db = sqlite3.connect('database.db')
+    return threading.current_thread().sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(threading.current_thread(), 'sqlite_db'):
+        threading.current_thread().sqlite_db.close()
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        usuario = mongo.db.usuarios
-        login_user_dict = usuario.find_one({'nombre_usuario' : request.form['username']})
+        conn = get_db()
+        c = conn.cursor()
+        c.execute("SELECT * FROM usuarios WHERE nombre_usuario=?", (request.form['username'],))
+        login_user_dict = c.fetchone()
 
         if login_user_dict:
-            if check_password_hash(login_user_dict['password'], request.form['password']):
-                user_obj = User(login_user_dict['nombre_usuario'])
+            if check_password_hash(login_user_dict[3], request.form['password']):
+                user_obj = User(login_user_dict[2])
                 login_user(user_obj)
-                if login_user_dict['tipo'] == 'alumno':
+                if login_user_dict[1] == 'alumno':
                     return redirect(url_for('solicitud'))
-                elif login_user_dict['tipo'] == 'comision':
+                elif login_user_dict[1] == 'comision':
                     return redirect(url_for('comentarios'))
-                elif login_user_dict['tipo'] == 'administrador':
+                elif login_user_dict[1] == 'administrador':
                     return redirect(url_for('administracion'))
             else:
                 flash('Contraseña incorrecta')
@@ -59,77 +74,118 @@ def login():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        usuarios = mongo.db.usuarios
-        existing_user = usuarios.find_one({'nombre_usuario' : request.form['username']})
+        username = request.form['username']
+        password = request.form['password']
+        tipo = request.form['tipo']
+        conn = get_db()
+        c = conn.cursor()
+        c.execute("SELECT * FROM usuarios WHERE nombre_usuario=?", (username,))
+        existing_user = c.fetchone()
 
         if existing_user is None:
-            hashpass = generate_password_hash(request.form['password'])
-            usuarios.insert_one({'nombre_usuario' : request.form['username'], 'password' : hashpass, 'tipo' : request.form['tipo']})
+            hashpass = generate_password_hash(password)
+            conn = get_db()
+            c = conn.cursor()
+            c.execute("INSERT INTO usuarios (nombre_usuario, password, tipo) VALUES (?, ?, ?)", (username, hashpass, tipo))
+            conn.commit()
             flash('Usuario registrado')
+            conn.close()
             return redirect(url_for('login'))
+        
+        conn.close()
         flash('El nombre de usuario ya existe')
         return redirect(url_for('register'))
+    
     return render_template('register.html')
 
 @app.route('/solicitud', methods=['GET', 'POST'])
 @login_required
 def solicitud():
     if request.method == 'POST':
-        solicitudes = mongo.db.solicitudes
-        solicitudes.insert_one({'apellidos_y_nombre_alumno' : request.form['nombre'], 'codigo_asignatura_uco' : request.form['asignatura_uco'], 'codigo_asignatura_extranjero' : request.form['asignatura_extranjero'], 'codigo_solicitud' : solicitudes.count_documents({}) + 1, 'fecha_solicitud' : datetime.datetime.now()})
-        flash('Solicitud enviada')
-        return redirect(url_for('solicitud'))
-    return render_template('solicitud.html', solicitudes=mongo.db.solicitudes.find({'apellidos_y_nombre_alumno' : current_user.id}), asignaturas_uco=mongo.db.asignaturas_uco.find(), asignaturas_exterior=mongo.db.asignaturas_exterior.find())
+        with sqlite3.connect('database.db') as conn:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO solicitudes(apellidos_y_nombre_alumno, codigo_asignatura_uco, codigo_asignatura_extranjero, codigo_solicitud, fecha_solicitud) VALUES (?, ?, ?, ?, ?)", (request.form['nombre'], request.form['asignatura_uco'], request.form['asignatura_extranjero'], cursor.execute('SELECT COUNT(*) FROM solicitudes').fetchone()[0] + 1, datetime.datetime.now()))
+            conn.commit()
+            flash('Solicitud enviada')
+            return redirect(url_for('solicitud'))
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        solicitudes = cursor.execute("SELECT * FROM solicitudes WHERE apellidos_y_nombre_alumno = ?", (current_user.get_id(),)).fetchall()
+        asignaturas_uco = cursor.execute("SELECT * FROM asignaturas_uco").fetchall()
+        asignaturas_exterior = cursor.execute("SELECT * FROM asignaturas_exterior").fetchall()
+    return render_template('solicitud.html', solicitudes=solicitudes, asignaturas_uco=asignaturas_uco, asignaturas_exterior=asignaturas_exterior)
 
 @app.route('/solicitud/<id>/eliminar', methods=['GET'])
 @login_required
 def eliminar_solicitud(id):
-    solicitudes = mongo.db.solicitudes
-    result = solicitudes.delete_one({'_id': ObjectId(id)})
-    if result.deleted_count == 1:
-        flash('Solicitud eliminada')
-    else:
-        flash('No se pudo eliminar la solicitud')
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM solicitudes WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+
+    flash('Solicitud eliminada')
     return redirect(url_for('solicitud'))
 
 def get_nombre_asignatura_uco(codigo_asignatura_uco):
-    asignaturas_uco = mongo.db.asignaturas_uco
-    asignatura_uco = asignaturas_uco.find_one({'codigo_asignatura_uco': codigo_asignatura_uco})
+    conn = sqlite3.connect('database.db')
+    conn = get_db()
+    c = conn.cursor()
+    c.execute("SELECT nombre_uco FROM asignaturas_uco WHERE codigo_asignatura_uco=?", (codigo_asignatura_uco,))
+    asignatura_uco = c.fetchone()
+    conn.close()
     if asignatura_uco:
-        return asignatura_uco['nombre_uco']
+        return asignatura_uco[0]
     else:
         return None
-app.jinja_env.globals.update(get_nombre_asignatura_uco=get_nombre_asignatura_uco)
-    
-def get_nombre_asignatura_exterior(codigo_asignatura_extranjero):
-    asignaturas_uco = mongo.db.asignaturas_exterior
-    asignatura_uco = asignaturas_uco.find_one({'codigo_asignatura_extranjero': codigo_asignatura_extranjero})
-    if asignatura_uco:
-        return asignatura_uco['nombre_extranjero']
-    else:
-        return None
-app.jinja_env.globals.update(get_nombre_asignatura_exterior=get_nombre_asignatura_exterior)
 
+app.jinja_env.globals.update(get_nombre_asignatura_uco=get_nombre_asignatura_uco)
+
+def get_nombre_asignatura_exterior(codigo_asignatura_extranjero):
+    conn = sqlite3.connect('database.db')
+    conn = get_db()
+    c = conn.cursor()
+    c.execute("SELECT nombre_extranjero FROM asignaturas_exterior WHERE codigo_asignatura_extranjero = ?", (codigo_asignatura_extranjero,))
+    result = c.fetchone()
+    conn.close()
+    if result:
+        return result[0]
+    else:
+        return None
+
+app.jinja_env.globals.update(get_nombre_asignatura_exterior=get_nombre_asignatura_exterior)
 
 @app.route('/comentarios', methods=['GET', 'POST'])
 @login_required
 def comentarios():
     if request.method == 'POST':
-        solicitudes = mongo.db.solicitudes
-        solicitudes.update_one({'_id' : ObjectId(request.form['_id'])}, {'$set' : {'comentarios' : request.form['comentarios']}})
+        conn = sqlite3.connect('database.db')
+        conn = get_db()
+        c = conn.cursor()
+        c.execute('UPDATE solicitudes SET comentarios = ? WHERE id = ?', (request.form['comentarios'], request.form['_id']))
+        c.commit()
         flash('Comentarios guardados')
         return redirect(url_for('comentarios'))
-    return render_template('comentarios.html', solicitudes=mongo.db.solicitudes.find())
+    conn = sqlite3.connect('database.db')
+    cur = c.execute('SELECT * FROM solicitudes')
+    solicitudes = cur.fetchall()
+    return render_template('comentarios.html', solicitudes=solicitudes)
 
 @app.route('/administracion', methods=['GET', 'POST'])
 @login_required
 def administracion():
     if request.method == 'POST':
-        solicitudes = mongo.db.solicitudes
-        solicitudes.update_one({'_id' : ObjectId(request.form['_id'])}, {'$set' : {'estado' : request.form['estado']}})
-        flash('Solicitud actualizada')
-        return redirect(url_for('administracion'))
-    return render_template('administracion.html', solicitudes=mongo.db.solicitudes.find())
+        with sqlite3.connect('database.db') as con:
+            cur = con.cursor()
+            cur.execute('UPDATE solicitudes SET estado=? WHERE _id=?', (request.form['estado'], request.form['_id']))
+            con.commit()
+            flash('Solicitud actualizada')
+            return redirect(url_for('administracion'))
+    with sqlite3.connect('database.db') as con:
+        cur = con.cursor()
+        cur.execute('SELECT * FROM solicitudes')
+        solicitudes = cur.fetchall()
+    return render_template('administracion.html', solicitudes=solicitudes)
 
 @app.route('/logout')
 @login_required
@@ -138,4 +194,4 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+# Crear la conexión con la base de datos
+conn = sqlite3.connect('app.db')
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Cerrar la conexión con la base de datos
+conn.close()
